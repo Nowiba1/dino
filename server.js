386e5d540135f3f81e3c1fcf030cd17ed45850e8
Nowiba1 <mohamedlabido9981@gmail.com>
2026-05-21 17:37:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const path = require("path")
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
-})
-app.use(express.static(path.join(__dirname, ".")));
-
+const io = new Server(server, {
+  cors: { 
+    origin: "https://nowiba1.github.io", 
+    methods: ["GET", "POST"] 
+  }
+});
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
