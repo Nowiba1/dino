ba6b0f2fb2398fa50c850d51abf2029584653f80
Nowiba1 <mohamedlabido9981@gmail.com>
2026-05-21 17:32:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const server = http.createServer(app)
 const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST"] }
 })
+app.use(express.static(path.join(__dirname, ".")));
 
-app.use(express.static(path.join(__dirname, "public")))
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 // ── Room State ────────────────────────────────────────────────────────────────
 const rooms = {}
 
